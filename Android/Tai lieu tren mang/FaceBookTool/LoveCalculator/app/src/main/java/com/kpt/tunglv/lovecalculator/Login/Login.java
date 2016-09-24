2aee43cfd1589226ac8ebdf74c27744836d531e8
TungLV <tungbi2111@gmail.com>
2016-09-24 17:05:35 +0700

    update

--- Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/Login/Login.java
+++ Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/Login/Login.java
@@ -4,12 +4,15 @@ package com.kpt.tunglv.lovecalculator.Login;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.kpt.tunglv.lovecalculator.R;
+import com.kpt.tunglv.lovecalculator.Result.Result;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,7 +40,12 @@ public class Login extends Fragment {
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Result result=new Result();
+                fragmentTransaction.replace(R.id.fragment_place, result);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
     }
--- Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/Splash/Splash.java
+++ Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/Splash/Splash.java
@@ -1,12 +1,15 @@
 package com.kpt.tunglv.lovecalculator.Splash;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.kpt.tunglv.lovecalculator.Login.Login;
 import com.kpt.tunglv.lovecalculator.R;
 
 /**
@@ -25,6 +28,34 @@ public class Splash extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        /****** Create Thread that will sleep for 3 seconds *************/
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 3 seconds
+                    sleep(3 * 1000);
+                    // After 3 seconds redirect to another intent
+                    android.support.v4.app.FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    Login login = new Login();
+                    fragmentTransaction.replace(R.id.fragment_place, login);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                    //Remove activity
+                } catch (Exception e) {
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
     }
 }
 
