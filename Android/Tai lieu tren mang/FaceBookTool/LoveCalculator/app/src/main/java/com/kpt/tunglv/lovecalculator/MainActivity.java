cf63f534282fca4e8ec4371852b09301c11f7eab
TungLV <tungbi2111@gmail.com>
2016-09-24 16:40:58 +0700

    update

--- Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/MainActivity.java
+++ Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/MainActivity.java
@@ -2,9 +2,9 @@ package com.kpt.tunglv.lovecalculator;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
 import com.kpt.tunglv.lovecalculator.Login.Login;
 import com.kpt.tunglv.lovecalculator.Splash.Splash;
@@ -19,8 +19,29 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         Splash splash = new Splash();
-        fragmentTransaction.replace(R.id.fragment_place, splash);
-        fragmentTransaction.commit();
+//        fragmentTransaction.replace(R.id.fragment_place, splash);
+//        fragmentTransaction.commit();
+        /****** Create Thread that will sleep for 3 seconds *************/
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 3 seconds
+                    sleep(3 * 1000);
+                    // After 3 seconds redirect to another intent
+                    Intent i = new Intent(getBaseContext(), Login.class);
+                    startActivity(i);
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
 
     }
 }
