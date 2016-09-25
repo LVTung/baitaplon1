8573eea290826b8b66739e6a7cf430f7289132c4
TungLV <tungbi2111@gmail.com>
2016-09-25 08:34:29 +0700

    update

--- Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/Login/Login.java
+++ Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/Login/Login.java
@@ -10,16 +10,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.kpt.tunglv.lovecalculator.API.NetworkUtil;
+import com.kpt.tunglv.lovecalculator.DebugLog.DebugLog;
 import com.kpt.tunglv.lovecalculator.R;
 import com.kpt.tunglv.lovecalculator.Result.Result;
+import com.kpt.tunglv.lovecalculator.Result.Result2;
+import com.kpt.tunglv.lovecalculator.Result.Result3;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Login extends Fragment {
     Button btnCalculate;
-
+    EditText edtYourName;
+    EditText edtLovesName;
 
     public Login() {
         // Required empty public constructor
@@ -31,22 +48,70 @@ public class Login extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
-           }
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        btnCalculate=(Button)view.findViewById(R.id.btnFCalculate);
+        btnCalculate = (Button) view.findViewById(R.id.btnFCalculate);
+        edtYourName = (EditText) view.findViewById(R.id.edtFLoginYourName);
+        edtLovesName = (EditText) view.findViewById(R.id.etdFYourlovename);
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String strYourName = edtYourName.getText().toString();
+                String strLovesName = edtLovesName.getText().toString();
+                requestInput(strYourName, strLovesName);
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                Result result=new Result();
+                Result result = new Result();
+                Result2 result2 = new Result2();
+                Result3 result3 = new Result3();
                 fragmentTransaction.replace(R.id.fragment_place, result);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
             }
         });
     }
+
+    private void requestInput(final String yourname, final String lovesname) {
+        String url = "https://love-calculator.p.mashape.com/getPercentage";
+        StringRequest InputRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        DebugLog.i(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        DebugLog.i(error.toString());
+                    }
+                }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("fname", yourname);
+                params.put("sname", lovesname);
+                return params;
+            }
+        };
+        NetworkUtil.getsInstance(getActivity().getApplicationContext()).addToRequestQueue(InputRequest);
+    }
+
+    public static String TOKEN = "";
+
+    public void onResponse(String response) {
+        DebugLog.i(response);
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            TOKEN = jsonObject.getString("token");
+            DebugLog.i("TOKEN" + TOKEN);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    ;
 }
--- Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/MainActivity.java
+++ Android/Tai lieu tren mang/FaceBookTool/LoveCalculator/app/src/main/java/com/kpt/tunglv/lovecalculator/MainActivity.java
@@ -1,41 +1,24 @@
 package com.kpt.tunglv.lovecalculator;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.kpt.tunglv.lovecalculator.Splash.Splash;
 
+public class MainActivity extends AppCompatActivity {
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FragmentManager fragmentManager = getFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        Splash splash = new Splash();
-//        fragmentTransaction.replace(R.id.fragment_place, splash);
-//        fragmentTransaction.commit();
-//        /****** Create Thread that will sleep for 3 seconds *************/
-//        Thread background = new Thread() {
-//            public void run() {
-//
-//                try {
-//                    // Thread will sleep for 3 seconds
-//                    sleep(3 * 1000);
-//                    // After 3 seconds redirect to another intent
-//                    Intent i = new Intent(getBaseContext(), Login.class);
-//                    startActivity(i);
-//                    //Remove activity
-//                    finish();
-//
-//                } catch (Exception e) {
-//
-//                }
-//            }
-//        };
-//
-//        // start thread
-//        background.start();
+        FragmentManager fragmentManager = getFragmentManager();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        Splash splash = new Splash();
+        fragmentTransaction.replace(R.id.fragment_place, splash).commit();
+
 
     }
 }
